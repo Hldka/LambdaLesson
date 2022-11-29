ae0174ccf833d83240e7586362731b593b1b519e
Hldka <116027001+Hldka@users.noreply.github.com>
2022-11-29 19:43:00 +0100

    first commit

--- src/lambdapractice/github.java
+++ src/lambdapractice/github.java
@@ -3,5 +3,6 @@ package lambdapractice;
 public class github {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hallo welt  ne mutlu sana");
     }
 }
